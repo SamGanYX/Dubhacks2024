6137587a91adc5e6c485bf4c3cbacdeb7e438477
SamGanYX <samganyx@gmail.com>
2024-12-29 16:22:40 -0800

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -24,9 +24,7 @@ const db = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    ssl: {
-        rejectUnauthorized: false // Disables SSL certificate validation
-      }
+    ssl: false
     // database: process.env.DB_NAME,
     // ssl: {
     //     ca: fs.readFileSync('./ca-cert.pem'),
